29c3946d5fb6a96962646fc7800873e01f56fdca
Berlinkg <nadyrbekov570@gmail.com>
2023-06-09 16:24:46 +0600

    Retrying requests

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+iimport 'package:http/http.dart' as http;
+import 'package:http/retry.dart';
+
+Future<void> main() async {
+  final client = RetryClient(http.Client());
+  try {
+    print(await client.read(Uri.http('example.org', '')));
+  } finally {
+    client.close();
+  }
+}
